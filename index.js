0283bf1dd7baa7f701a9cdb69995180728c9248d
Juan Ignacio Penzo <juanipenzo97@gmail.com>
2022-09-27 19:11:51 -0300

    27/09/2022

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-/*Condicional*/
+/*Condicional
 alert("Debe elegir entre las siguientes opciones")
-alert("LEBAC: 100% TEA, Plazo Fijo: 85% TEA, Bono Soberano, 90% TEA")
 
 let valor = parseInt(prompt("Ingrese el monto dispuesto a invertir"));
-let inversion = prompt("Ingrese la inversion deseada")
+let inversion = prompt(`¿Que inversion desea simular? \n [1] LEBAC - 100% TEA \n [2] Plazo Fijo - 85% TEA \n [3] Bono Soberano - 90% TEA \n [S] Salir`)
 let tiempo = parseInt(prompt("Ingrese los dias dispuesto a invertir"))
 
 function intereses(inversion){
     switch (inversion) {
-        case "LEBAC":
+        case "1":
             return 2;
-        case "Plazo Fijo":
+        case "2":
             return 1.85;
-        case "Bono Soberano":
+        case "3":
             return 1.90;
+        case "S":
+            return "No valido";
     }
 }
 
 alert("Usted recibira al final $" + (valor * intereses(inversion)* (tiempo/365) + valor))
 
-/*Ciclos*/
+/*Ciclos
 alert("¿Cuantos dias de inversion necesito para alzanzar mi objetivo?");
-alert("LEBAC: 100% TEA, Plazo Fijo: 85% TEA, Bono Soberano, 90% TEA")
 
 let valor2 = parseInt(prompt("Ingrese el monto dispuesto a invertir"));
 let valorDefinido = parseInt(prompt("Ingrese el monto dispuesto a llegar"));
-let inversion2 = prompt("Ingrese la inversion deseada");
+let inversion2 = prompt(`¿Que inversion desea simular? \n [1] LEBAC - 100% TEA \n [2] Plazo Fijo - 85% TEA \n [3] Bono Soberano - 90% TEA \n [S] Salir`)
 let i = 0;
 
 do{
